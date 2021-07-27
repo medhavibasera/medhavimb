92e4bf279a7fc9d6318f48d23cefb827cac24d91
Medhavi Basera <1905781@kiit.ac.in>
2021-07-27 20:24:14 +0530

    video added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
+import { useState, useEffect } from "react";
+
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Preloader from "./components/Preloader";
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000);
+  }, []);
+
   return (
-    <div className="App" style={{ width: "100vw" }}>
-      <Navbar />
-      <LandingPage />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      {loading ? (
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Preloader />
+        </div>
+      ) : (
+        <div className="App" style={{ width: "100vw" }}>
+          <Navbar />
+          <LandingPage />
+          <About />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/Preloader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Lottie from "react-lottie";
+import homeanimation from "../images/welcome.json";
+
+export default function Preloader() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: homeanimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div id="preloader">
+      <Lottie options={defaultOptions} height="70%" width="70%" />
+    </div>
+  );
+}
